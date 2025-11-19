2558f73fddc6b3cc122ed3b4fc6957a4003d850b
6godpro <erickson5115@gmail.com>
2025-11-19 21:35:07 +0100

    feat: added new schema to schemaTypes list

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,4 +1,5 @@
-import { eventType } from "./eventType";
-import { photoType } from "./photoType";
+import {eventType} from './eventType'
+import {photoType} from './photoType'
+import {upcomingEvent} from './upcomingEventType'
 
 export const schemaTypes = [eventType, photoType]
--- schemaTypes/upcomingEventType.ts
+++ schemaTypes/upcomingEventType.ts
@@ -1,35 +1,35 @@
-import { defineType, defineField } from "sanity";
+import {defineType, defineField} from 'sanity'
 
 export const upcomingEvent = defineType({
-  name: "upcomingEvent",
-  title: "Upcoming Event",
-  type: "document",
+  name: 'upcomingEvent',
+  title: 'Upcoming Event',
+  type: 'document',
   fields: [
     defineField({
-      name: "title",
-      type: "string",
-      title: "Event Title",
+      name: 'title',
+      type: 'string',
+      title: 'Event Title',
       validation: (rule) => rule.required(),
     }),
 
     defineField({
-      name: "date",
-      type: "datetime",
-      title: "Event Date",
+      name: 'date',
+      type: 'datetime',
+      title: 'Event Date',
       validation: (rule) => rule.required(),
     }),
 
     defineField({
-      name: "description",
-      type: "text",
-      title: "Description",
+      name: 'description',
+      type: 'text',
+      title: 'Description',
     }),
 
     defineField({
-      name: "banner",
-      type: "image",
-      title: "Event Banner",
-      options: { hotspot: true },
+      name: 'banner',
+      type: 'image',
+      title: 'Event Banner',
+      options: {hotspot: true},
     }),
   ],
-});
+})
